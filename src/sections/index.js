59eb44c2a90717492757c64ebe3874b520644874
Mohommed Dastagir Shaikh <dastagirshaikh9519@gmail.com>
2024-06-25 11:20:34 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+    Services,
+    PopularProducts,
+    CustomerReviews,
+    Footer
+}
